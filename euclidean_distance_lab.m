b28c68f53edad0411cf1b987e22d03a9c2cd17fb
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-01-26 16:17:50 +0100

    lab finished

---
+++ euclidean_distance_lab.m
@@ -0,0 +1,16 @@
+function dE = euclidean_distance_lab(V1, V2)
+    arguments
+        V1 (3,20) {mustBeNumeric, mustBeNumeric}
+        V2 (3,20) {mustBeNumeric, mustBeNumeric}
+    end
+    L1 = V1(1,:);
+    L2 = V2(1,:);
+
+    a1 = V1(2,:);
+    a2 = V2(2,:);
+
+    b1 = V1(3,:);
+    b2 = V2(3,:);
+
+    dE = sqrt((L1-L2).^2 + (a1-a2).^2 + (b1-b2).^2);
+end
